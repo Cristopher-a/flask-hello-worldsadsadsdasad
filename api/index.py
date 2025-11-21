525607a07e2bf87d66a4207c066771d0324891e6
Cristopher <150216503+Cristopher-a@users.noreply.github.com>
2025-11-20 23:03:33 -0600

    Clean up imports and remove Supabase client
    Removed unused imports and Supabase client initialization.

--- api/index.py
+++ api/index.py
@@ -1,15 +1,8 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-import requests
-from requests.auth import HTTPBasicAuth
-from supabase import create_client, Client
 
 app = Flask(__name__)
 CORS(app)  # ← Habilita CORS en todo
-
-SUPABASE_URL = "https://rhttqmtzcmwilzshnxwq.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJodHRxbXR6Y213aWx6c2hueHdxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM1OTUwOTAsImV4cCI6MjA3OTE3MTA5MH0.8dYvM8CBEdqiF9ZZhaYRKhtOin_wYGf4JYrmTTIsX74"
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 @app.route('/')
 def home():
