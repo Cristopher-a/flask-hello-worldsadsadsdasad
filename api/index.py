fb917b427629745415a61d22556335b9221b9934
Cristopher <150216503+Cristopher-a@users.noreply.github.com>
2025-11-20 22:54:37 -0600

    Enable CORS for the Flask app
    Added CORS support to the Flask application.

--- api/index.py
+++ api/index.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # ← Habilita CORS en todo
 
 @app.route('/')
 def home():
